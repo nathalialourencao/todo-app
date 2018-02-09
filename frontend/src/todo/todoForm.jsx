fcd2d624e0a1f07d9df57d65ee1fdbfca12e12d4
nahlourencao <nahlourencao@gmail.com>
2018-02-09 18:54:19 -0200

    Componentes Grid e Button

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Grid from '../templates/grid'
+import Button from '../templates/Button'
 
 export default props => (
-    <div>
-        <h1>Form</h1>
+    <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa'/>
+        </Grid>
+        <Grid cols='12 3 2'>
+            <Button style='primary' icon='plus'></Button>
+        </Grid>
     </div>
 )
