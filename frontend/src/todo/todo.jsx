70c59238994fce488a486891c08129055386694c
nahlourencao <nahlourencao@gmail.com>
2018-02-09 19:28:34 -0200

    Evento onChange e Integração com a API

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,23 +1,42 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+
 import PageHeader from '../templates/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component{
     constructor(props){
         super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
 
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+
     handleAdd(){
-        console.log(this)
+        console.log(this.state.description)
+
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => console.log('Funcionou'))
     }
 
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoForm
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}
+                />
                 <TodoList/>
             </div>
         )
