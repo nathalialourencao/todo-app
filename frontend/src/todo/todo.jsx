f742c92c0abb603db3d24c33371ca50ed7100d76
nahlourencao <nahlourencao@gmail.com>
2018-02-13 17:23:32 -0200

    Consulta e exclusão de TODOs

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,6 +14,14 @@ export default class Todo extends Component{
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
     }
 
     handleChange(e){
@@ -25,7 +33,12 @@ export default class Todo extends Component{
 
         const description = this.state.description
         axios.post(URL, {description})
-            .then(resp => console.log('Funcionou'))
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     render(){
@@ -37,7 +50,10 @@ export default class Todo extends Component{
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd}
                 />
-                <TodoList/>
+                <TodoList
+                    list={this.state.list}
+                    handleRemove={this.handleRemove}
+                />
             </div>
         )
     }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import Button from '../templates/Button'
 
-export default props =>(
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+
+        return list.map(todo => (
+            <tr key={todo._id}>
+                 <td>
+                    {todo.description}
+                </td>
+                <td>
+                    <Button style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)}></Button>
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+            <tr>
+                <th>Descrição</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            {renderRows()}
+            </tbody>
+        </table>
+    )
+}
