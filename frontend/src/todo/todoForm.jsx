14c10d9858783605ba17199daea8a880a0e00a58
nahlourencao <nahlourencao@gmail.com>
2018-02-13 23:07:39 -0200

    Conectando o TodoList e TodoForm com Redux

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Grid from '../templates/grid'
 import Button from '../templates/Button'
 
-export default props => {
+const TodoForm = props => {
 
     const keyHandler = (e) => {
         if (e.key === 'Enter'){
@@ -30,3 +32,6 @@ export default props => {
         </div>
     )
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+export default connect(mapStateToProps)(TodoForm)
